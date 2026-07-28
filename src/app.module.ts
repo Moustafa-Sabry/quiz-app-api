1947816aca34cfc89c4d63aa67a37de07635e6a5
Moustafa-Sabry <133038389+Moustafa-Sabry@users.noreply.github.com>
2026-07-28 18:30:06 +0300

    Initial empty main branch

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { GroupsModule } from './modules/groups/groups.module';
+import { QuestionsModule } from './modules/questions/questions.module';
+import { QuizzesModule } from './modules/quizzes/quizzes.module';
+import { ResultsModule } from './modules/results/results.module';
+import { StudentsModule } from './modules/students/students.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+
+@Module({
+  imports: [AuthModule, GroupsModule, QuestionsModule, QuizzesModule, ResultsModule, StudentsModule, DashboardModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
